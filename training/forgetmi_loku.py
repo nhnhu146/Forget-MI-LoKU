e597ee4e3776e3ff00e7f40c6ac2a371ccf284ef
Nguyen Hoang Nhu <nhnhu22@clc.fitus.edu.vn>
2026-05-21 15:40:50 +0700

    Sửa lỗi module 'torch' has no attribute 'manual_seed_all'

--- training/forgetmi_loku.py
+++ training/forgetmi_loku.py
@@ -43,7 +43,7 @@ def set_seed(seed: int):
     np.random.seed(seed)
     torch.manual_seed(seed)
     if torch.cuda.is_available():
-        torch.manual_seed_all(seed)
+        torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
